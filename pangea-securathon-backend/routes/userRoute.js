dee0b685e069c95b5a96ad7c3d744a961bc16a88
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-05-29 22:13:08 +0530

    creating user api added and mit license added

--- pangea-securathon-backend/routes/userRoute.js
+++ pangea-securathon-backend/routes/userRoute.js
@@ -5,14 +5,38 @@ const user = require("../models/userModel");
 // this route will create new user
 router.post("/createUser", async (req,res)=>{
     const {fName,lName, email, password} = req.body;
-    console.log({fName,lName, email,password});
-    const newUser = new user({
-
-    });
-    await newUser.save();
-    res.json({
-        "user data" : newUser
-    })
+    const findUser = await user.findOne({email:email});
+    let msg ;
+    let detailMsg;
+    //first find user if exists
+    if (findUser) {
+        msg = "user exists";
+        detailMsg = "This user exists. Please login"
+        res.json({
+            "msg" : msg,
+            "detail msg" : detailMsg,
+            "success" : false
+        })
+    }else{
+        // when testing is done encrypt their password
+        const newUser = new user({
+           email : email,
+           userDetails : {
+            fName: fName,
+            lName : lName,
+            password : password
+           }
+        });
+        await newUser.save();
+        //find that user and send back without password
+        const savedUser = await user.findById(newUser._id).select("-userDetails.password");
+        msg = "new user created";
+        res.json({
+            "msg" : msg,
+            "success" : true,
+            "user data" : savedUser
+        })
+    }
 });
 
 
